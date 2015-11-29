bb9334adf457cb3fdd06a019aa00d4b46e17aec4
aStaCruz <albert.stacruz@gmail.com>
2015-11-28 17:10:27 -0800

    started on the item class, need to work on the image buttons being parallel to an item array

--- app/src/main/java/albert/beta/home.java
+++ app/src/main/java/albert/beta/home.java
@@ -15,7 +15,7 @@ import android.view.View.OnClickListener;
 public class home extends AppCompatActivity {
 
     //create variable pointers
-    private ImageView appImageView;
+    private ImageView _recipeView;
     private ImageButton _imgB1;
     private ImageButton _imgB2;
     private ImageButton _imgB3;
@@ -23,26 +23,39 @@ public class home extends AppCompatActivity {
     private Drawable draw;
     private Random random;
     private Drawable [] drawables = null;
+    private _Item [] _recipes = null;
+    private _Item []  _berserkerRecipe = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        //test data now
         //SAYS EVERY DATA IN THIS ARRAY IS NOW NULL INSTEAD OF ACTUAL PICTURES BEFORE
         drawables = new Drawable[] {
                 getResources().getDrawable(R.mipmap.ancient_coin_item),
                 getResources().getDrawable(R.mipmap.boots_of_speed_item),
                 getResources().getDrawable(R.mipmap.dagger_item),
                 getResources().getDrawable(R.mipmap.berserkers_greaves_item)
-        };; // create a Drawables array that stores location of different images
+        }; // create a Drawables array that stores location of different images
+
+        _berserkerRecipe = new _Item[] {
+                new _Item("dagger", null, getResources().getDrawable(R.mipmap.dagger_item)),
+                new _Item("boots of speed", null, getResources().getDrawable(R.mipmap.boots_of_speed_item))
+        };
+
+        _recipes = new _Item [] {
+               new _Item("berserker's greaves", _berserkerRecipe, getResources().getDrawable(R.mipmap.berserkers_greaves_item))
+        };
+
 
         //assign pointer data
         _imgB1 = (ImageButton) findViewById(R.id.imgButton1); //set pointer to img button1
         _imgB2 = (ImageButton) findViewById(R.id.imgButton2);
         _imgB3 = (ImageButton) findViewById(R.id.imgButton3);
         _startGame = (Button) findViewById(R.id.start_game);
-
+        _recipeView = (ImageView) findViewById(R.id.recipe);
 
         //on button click set image button to random image in the array
         _imgB1.setOnClickListener(new View.OnClickListener()
@@ -50,10 +63,7 @@ public class home extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
-                random = new Random();
-                int rand = random.nextInt(drawables.length);
-                draw = drawables[rand];
-                _imgB1.setImageDrawable(draw); // set the image to the ImageView
+
             }
         });
 
@@ -63,6 +73,13 @@ public class home extends AppCompatActivity {
             public void onClick(View view)
             {
                 _startGame.setVisibility(View.GONE);
+
+                random = new Random();
+                int rand = random.nextInt(_recipes.length);
+                draw = _recipes[rand].get_image();
+                _recipeView.setImageDrawable(draw);
+
+
             }
         });
     }
